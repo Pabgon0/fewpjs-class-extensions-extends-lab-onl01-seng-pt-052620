9ad2d4a039e527622c0f8a1f0ff85b120fd98198
Pablo Gonzalez <pabgon0@gmail.com>
2020-12-31 19:08:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code here
+class Polygon {
+  constructor(arr) {
+    this.arr = arr
+  }
+  
+  get countSides() {
+    return arr.length
+  }
+  
+  get perimeter() {
+    sum = 0
+    arr.each += sum
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    
+  }
+}
